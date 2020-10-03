01a93c6476d87cf2a6670e672ee8eeebd85cb754
Priyanshi <priyanshigahlawat11@gmail.com>
2020-10-03 05:45:50 +0530

    version 1

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.burger')
+const navbar = document.querySelector('.navbar')
+const navList = document.querySelector('.nav-list')
+const rightNav = document.querySelector('.right-nav')
+
+burger.addEventListener('click',()=>{
+       console.log('hello');
+       rightNav.classList.toggle('v-class-resp');
+       navList.classList.toggle('v-class-resp');
+       navbar.classList.toggle('h-nav-resp');
+})
+
+
